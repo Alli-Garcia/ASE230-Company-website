b34491e15ae6f398791f9eb3b47e2c72373bc575
NoirJazz <91505353+NoirJazz@users.noreply.github.com>
2023-09-22 13:22:31 -0400

    Testing function to read plain text

---
+++ 02/lib/plain text.php
@@ -0,0 +1,18 @@
+// A function for reading a plain text file
+
+<?php
+function read_file($file)
+{
+    $fp = fopen($file, 'r');
+    $data = fread($fp, filesize($file));
+    fclose($fp);
+    return $data;
+}
+
+function testReadFile()
+{
+    $expected = "This is a plain text file.";
+    $actual = read_file("test.txt");
+    $this->assertEquals($expected, $actual);
+}
+?>
