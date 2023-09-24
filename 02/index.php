9740c7fe6dc5c529a2d91af7a36fdce0ee813253
NoirJazz <91505353+NoirJazz@users.noreply.github.com>
2023-09-24 18:35:16 -0400

    Changed site name and removed pricing section

--- 02/index.php
+++ 02/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Hiric</title>
+    <title>NaturaTech</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Premium Bootstrap 5 Landing Page Template" />
     <meta name="keywords" content="bootstrap 5, premium, marketing, multipurpose" />
@@ -403,70 +403,6 @@
         </div>
     </section>
     <!--END ABOUT-US-->
-
-    <!--START PRICING-->
-    <section class="section bg-light" id="pricing">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 offset-lg-2">
-                    <h1 class="section-title text-center">Our Pricing</h1>
-                    <div class="section-title-border mt-3"></div>
-                    <p class="section-subtitle font-secondary text-muted text-center pt-4">Call to action pricing table
-                        is really crucial to your for your business website. Make your bids stand-out with amazing
-                        options.</p>
-                </div>
-            </div>
-            <div class="row mt-5">
-                <div class="col-lg-4">
-                    <div class="text-center pricing-box">
-                        <h4 class="text-uppercase">Economy</h4>
-                        <h1>$9.90</h1>
-                        <h6 class="text-uppercase text-muted">Billing Per Month</h6>
-                        <div class="plan-features mt-5">
-                            <p>Bandwidth: <b class="text-primary">1GB</b></p>
-                            <p>Onlinespace: <b class="text-primary">50MB</b></p>
-                            <p>Support: <b class="text-primary">No</b></p>
-                            <p><b class="text-primary">1</b> Domain</p>
-                            <p><b class="text-primary">No</b> Hidden Fees</p>
-                        </div>
-                        <a href="#" class="btn btn-primary waves-effect waves-light mt-5">Join Now</a>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="text-center pricing-box price-active">
-                        <div class="ribbon-box"><span>Popular</span></div>
-                        <h4 class="text-uppercase">Deluxe</h4>
-                        <h1>$19.90</h1>
-                        <h6 class="text-uppercase text-muted">Billing Per Month</h6>
-                        <div class="plan-features mt-5">
-                            <p>Bandwidth: <b class="text-primary">10GB</b></p>
-                            <p>Onlinespace: <b class="text-primary">500MB</b></p>
-                            <p>Support: <b class="text-primary">Yes</b></p>
-                            <p><b class="text-primary">10</b> Domain</p>
-                            <p><b class="text-primary">No</b> Hidden Fees</p>
-                        </div>
-                        <a href="#" class="btn btn-primary waves-effect waves-light mt-5">Join Now</a>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="text-center pricing-box">
-                        <h4 class="text-uppercase">Ultimate</h4>
-                        <h1>$29.90</h1>
-                        <h6 class="text-uppercase text-muted">Billing Per Month</h6>
-                        <div class="plan-features mt-5">
-                            <p>Bandwidth: <b class="text-primary">100GB</b></p>
-                            <p>Onlinespace: <b class="text-primary">2 GB</b></p>
-                            <p>Support: <b class="text-primary">Yes</b></p>
-                            <p><b class="text-primary">Unlimited</b> Domain</p>
-                            <p><b class="text-primary">No</b> Hidden Fees</p>
-                        </div>
-                        <a href="#" class="btn btn-primary waves-effect waves-light mt-5">Join Now</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--ENd PRICING-->
 
     <!--START TESTIMONIAL-->
     <section class="section" id="testi">
