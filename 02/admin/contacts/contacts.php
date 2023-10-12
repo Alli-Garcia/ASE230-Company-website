cb3c8a2b0414b6280228c43864b464c820c30a7c
Alli <alligarcia2021@gmail.com>
2023-10-12 15:07:11 -0400

    Jk this is the actual test

--- 02/admin/contacts/contacts.php
+++ 02/admin/contacts/contacts.php
@@ -64,25 +64,4 @@ function delete($index) {
     }
 }
 
-function getContactRequests() {
-    $filePath = '../../data/realtors.json';
-
-    // Check if the file exists
-    if (!file_exists($filePath)) {
-        return []; // Return an empty array if the file doesn't exist
-    }
-
-    // Read the content of the JSON file
-    $jsonContent = file_get_contents($filePath);
-
-    // Decode the JSON content into an associative array
-    $data = json_decode($jsonContent, true);
-
-    // Check if the 'realtors' key exists in the decoded data
-    if (isset($data['realtors'])) {
-        return $data['realtors'];
-    } else {
-        return []; // Return an empty array if the 'realtors' key is not found
-    }
-}
 ?>
