f758982696ddb0af0750e01703a8c918eed5cdc4
NoirJazz <91505353+NoirJazz@users.noreply.github.com>
2023-10-12 18:18:52 -0400

    Detail page test

--- 02/admin/team/detail.php
+++ 02/admin/team/detail.php
@@ -1,1 +1,16 @@
-.
+<?php
+include_once 'team.php';
+
+if (isset($_GET['index'])) {
+    $index = $_GET['index'];
+    detail($index);
+    
+    // create edit button
+    echo '<div class="btn-group">';
+    echo '<a href="edit.php?index=' . $index . '" class="btn btn-primary">Edit</a>';
+    
+    // create delete button
+    echo '<a href="delete.php?index=' . $index . '" class="btn btn-danger">Delete</a>';
+    echo '</div>';
+}
+?>
