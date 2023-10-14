28154455ae17300816879431ba3a25d716fca504
Alli <alligarcia2021@gmail.com>
2023-10-13 21:19:06 -0400

    Creating auth folder

---
+++ 02/admin/auth/auth.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = 'localhost';
+$db = 'users';
+$user = 'root';
+$pass = 'Alli2002!';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
