ed733a691f564f4a28c35762b25734ce81ef6226
NoirJazz <91505353+NoirJazz@users.noreply.github.com>
2023-10-15 18:56:02 -0400

    Renamed instances of $index to $teamMember

--- 02/admin/team/delete.php
+++ 02/admin/team/delete.php
@@ -4,7 +4,7 @@ require('team.php');
 // Check if user has confirmed deletion
 if (isset($_POST['confirm']) && $_POST['confirm'] == 'yes') {
     // Get the index of the item to delete
-    $index = $_POST['index'];
+    $teamMember = $_POST['team_member'];
 
     // Load the team data from .csv file
     $teamMembers = array_map('str_getcsv', file('../../data/team.csv'));
@@ -14,7 +14,7 @@ if (isset($_POST['confirm']) && $_POST['confirm'] == 'yes') {
     array_shift($teamMembers);
 
     // Delete the item from the array
-    unset($teamMembers[$index]);
+    unset($teamMembers[$teamMember]);
 
     // Save the data to the file
     $file = fopen('../../data/team.csv', 'w');
@@ -25,12 +25,12 @@ if (isset($_POST['confirm']) && $_POST['confirm'] == 'yes') {
     }
     fclose($file);
 
-    // Redirect the index page   
+    // Redirect to index page   
     header('location: index.php');
     exit();
 }
 // Get the index of the item to delete
-$index = $_GET['index'];
+$teamMember = $_GET['team_member'];
 
 // Load the current team data from the file
 $teamMembers = array_map('str_getcsv', file('../../data/team.csv'));
@@ -40,7 +40,7 @@ array_walk($teamMembers, function (&$a) use ($teamMembers) {
 array_shift($teamMembers);
 
 // Get item to delete
-$item = $teamMembers[$index];
+$item = $teamMembers[$teamMember];
 
 // Display confirmation message to user
 echo '<p>Are you sure you wish to delete the following item(s)?</p>';
@@ -51,7 +51,7 @@ echo '<p><a href="team.php">Cancel</a> | <a href="#" onclick="document.getElemen
 
 // Display a form to submit the confirmation
 echo '<form id="delete-form" method="post">';
-echo '<input type="hidden" name="index" value="' . $index . '">';
+echo '<input type="hidden" name="index" value="' . $teamMember . '">';
 echo '<input type="hidden" name="confirm" value="yes">';
 echo '</form>';
 
