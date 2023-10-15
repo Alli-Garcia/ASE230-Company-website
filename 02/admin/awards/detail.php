5acfed7d72aef29097c4e74fcfb85924ff42d6c3
Kori McClure <35781456+ganmcc6nku@users.noreply.github.com>
2023-10-15 01:23:27 -0400

    Add files via upload
    added bootstrap 5 to the awards admin area

--- 02/admin/awards/detail.php
+++ 02/admin/awards/detail.php
@@ -7,12 +7,20 @@ detail($_POST['award']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Awards-Detail</title>
+</head>
 <body>
     <form action="../awards/edit.php" method='get'>
-	<button name="0" type="submit" value=<?php echo $_POST['award']//is int ?>>Edit</button></br>
+	<button class="btn btn-lg btn-outline-dark btn-primary" name="0" type="submit" value=<?php echo $_POST['award']//is int ?>>Edit</button></br>
 </form>
     <form action="../awards/delete.php" method='get'>
-    <button name="0" type="submit" value=<?php echo $_POST['award']//is int ?>>Delete</button>
+    <button class="btn btn-lg btn-outline-dark btn-primary" name="0" type="submit" value=<?php echo $_POST['award']//is int ?>>Delete</button>
 </form>
 </form>
 </body>
