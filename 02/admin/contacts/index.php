da68cedf7f01e94efed64409e7022ae973292a5b
Kori McClure <35781456+ganmcc6nku@users.noreply.github.com>
2023-10-15 20:56:57 -0400

    added bootstrap5

--- 02/admin/contacts/index.php
+++ 02/admin/contacts/index.php
@@ -13,6 +13,8 @@ $contactRequests = getContactRequests();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Requests</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
 
@@ -23,13 +25,14 @@ $contactRequests = getContactRequests();
             <th>ID</th>
             <th>Name</th>
             <th>Email</th>
-            <th>Actions</th>
+            <th>Bio</th>
         </tr>
         <?php foreach ($contactRequests as $index => $request): ?>
             <tr>
                 <td><?php echo $request['id']; ?></td>
                 <td><?php echo $request['name']; ?></td>
                 <td><?php echo $request['email']; ?></td>
+                <td><?php echo $request['bio']; ?></td>
                 <td>
                     <a href="detail.php?id=<?php echo $request['id']; ?>">View Details</a>
                 </td>
