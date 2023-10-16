3d9ea9b6586541ab7530e18697ebd9af6af7a096
Alli <alligarcia2021@gmail.com>
2023-10-15 21:04:09 -0400

    committing changes

--- 02/admin/auth/index.php
+++ 02/admin/auth/index.php
@@ -12,6 +12,7 @@ if (!isLoggedIn()) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Home Page</title>
 </head>
 <body>
@@ -22,5 +23,7 @@ if (!isLoggedIn()) {
     <form action="logout.php" method="post">
         <input type="submit" value="Logout">
     </form>
+    
 </body>
 </html>
+
